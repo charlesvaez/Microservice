b62398ae1408a1f8a300ee6f992f453b87a9b885
charlesvaez <839019311@qq.com>
2019-04-18 16:15:50 +0800

    springcloud-config

--- springcloud-config/springcloud-config-client/src/main/java/com/hgsoft/springcloud/config/client/SpringcloudConfigClientApplication.java
+++ springcloud-config/springcloud-config-client/src/main/java/com/hgsoft/springcloud/config/client/SpringcloudConfigClientApplication.java
@@ -1,10 +1,24 @@
 package com.hgsoft.springcloud.config.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+@RefreshScope
 public class SpringcloudConfigClientApplication {
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringcloudConfigClientApplication.class, args);
---
+++ springcloud-config/springcloud-config-server/doc/readMe.txt
@@ -0,0 +1,5 @@
+1. javax.net.ssl.SSLHandshakeException: sun.security.validator.ValidatorException: PKIX path building failed: sun.security.provider.certpath.SunCertPathBuilderException:  unable to find valid certification path to requested target
+
+   需要使用InstallCert类完成证书安装
+
+   https://www.oschina.net/question/12_19249
